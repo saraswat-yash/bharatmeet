f2c789b07c4c1b8f2e8bf9682516f2a7c419a5c3
Yash Saraswat <yash@Yashs-Mac.local>
2020-07-06 08:30:36 -0700

    New repo

---
+++ app/features/config/index.js
@@ -0,0 +1,48 @@
+
+export default {
+    /**
+     * The URL with extra information about the app / service.
+     */
+    aboutURL: 'https://bharatmeet.live',
+
+    /**
+     * The URL to the source code repository.
+     */
+    sourceURL: '#',
+
+    /**
+     * Application name.
+     */
+    appName: 'Bharat Meet',
+
+    /**
+    * The prefix for application protocol.
+    * You will also need to replace this in package.json.
+    */
+    appProtocolPrefix: 'bharatmeet',
+
+    /**
+     * The default server URL of Jitsi Meet Deployment that will be used.
+     */
+    defaultServerURL: 'https://join.bharatmeet.live',
+
+    /**
+     * The default server Timeout in seconds.
+     */
+    defaultServerTimeout: 120,
+
+    /**
+     * URL to send feedback.
+     */
+    feedbackURL: 'mailto:support@bharatmeet.live',
+
+    /**
+     * The URL of Privacy Policy Page.
+     */
+    privacyPolicyURL: 'https://bharatmeet.live/privacy-policy',
+
+    /**
+     * The URL of Terms and Conditions Page.
+     */
+    termsAndConditionsURL: 'https://bharatmeet.live/terms-of-service'
+};
---
+++ app/features/welcome/styled/Wrapper.js
@@ -0,0 +1,10 @@
+// @flow
+
+import styled from 'styled-components';
+
+export default styled.div`
+    background: #3369e7;
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+`;
